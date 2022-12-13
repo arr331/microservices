0f411fb79c28b6eb3e9deb03e7c1be35a786729a
Adrian Ramírez <adrirarm@suramericana.com.co>
2022-12-13 16:34:20 -0500

    SE AGREGA ZUUL AL GATEWAY, SE QUITAN DEPENDENCIAS Y SE BAJA DE VERSION

--- gateway-server/src/main/java/com/gateway/gatewayserver/GatewayServerApplication.java
+++ gateway-server/src/main/java/com/gateway/gatewayserver/GatewayServerApplication.java
@@ -3,8 +3,11 @@ package com.gateway.gatewayserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
 
 @SpringBootApplication
+@EnableZuulProxy
 @EnableDiscoveryClient
 public class GatewayServerApplication {
 
